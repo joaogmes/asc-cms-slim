528d4cc3668f6ae01dfe06a5da4b4f2021048b0f
Joao Gomes <joao-pedrogomes@hotmail.com>
2023-11-15 18:23:04 -0300

    List pages on pages js controller

--- app/view/scripts/manager/pages.js
+++ app/view/scripts/manager/pages.js
@@ -3,4 +3,26 @@ class Pages extends TemplateManager {
     super();
     console.log("Pages class");
   }
+
+  test() {
+    console.log("worked");
+    $("html").css("background", "red");
+  }
+
+  listPages() {
+    return new Promise((resolve, reject) => {
+      var pagesData = {
+        client_token: "123",
+      };
+
+      this.serviceRequest("/api/pages", pagesData, (result) => {
+        resolve(result);
+      });
+    });
+  }
 }
+
+var pageClass = new Pages();
+pageClass.listPages().then((pageList) => {
+  console.log(pageList);
+});
