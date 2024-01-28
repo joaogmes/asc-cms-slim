f53ea65ca13c6f1203c978df9d79c432c2ec0710
Joao Gomes <joao-pedrogomes@hotmail.com>
2024-01-28 17:52:12 -0300

    Parse user data to Bot class

--- app/view/scripts/bot.js
+++ app/view/scripts/bot.js
@@ -8,8 +8,6 @@ class MessageHandler {
   }
 
   async displayMessage(content, type) {
-    console.log("Displaying message:", content, type);
-
     const typeClass = type === "message" ? "received" : "sent";
     const timeout = typeClass === "received" ? 1500 : 0;
 
@@ -32,17 +30,13 @@ class MessageHandler {
 }
 
 class InputHandler {
-  constructor(inputElement, submitElement, { userData, messageHandler }) {
+  constructor(inputElement, submitElement) {
     this.inputElement = $(inputElement);
     this.submitElement = $(submitElement);
-    this.userData = userData;
-    this.messageHandler = messageHandler;
     this.currentField = null;
   }
 
   promptInput(mask, name) {
-    console.log("Prompting input:", mask, name);
-
     this.currentField = name;
 
     this.inputElement.attr("data-name", name);
@@ -58,24 +52,18 @@ class InputHandler {
     this.enableInput();
 
     return new Promise((resolve) => {
-      const handleButtonClick = (e) => {
+      const handleButtonClick = () => {
         const scopeName = this.currentField;
         const inputVal = this.inputElement.val();
 
         if (!inputVal) {
-          console.log("Input value is empty!");
           return false;
         }
 
-        this.userData[scopeName] = inputVal;
-        console.log("User data:", this.userData);
-
-        this.messageHandler.displayMessage(inputVal, "sent");
         this.disableInput();
-        resolve();
+        resolve({ [scopeName]: inputVal });
       };
 
-      // Remove todos os manipuladores de eventos de clique antes de adicionar um novo
       this.submitElement.off("click");
       this.submitElement.on("click", handleButtonClick);
     });
@@ -99,6 +87,7 @@ class Bot {
     this.inputHandler = inputHandler;
     this.messageHandler = messageHandler;
     this.currentStepIndex = 0;
+    this.userData = {};
   }
 
   async processConversation() {
@@ -110,14 +99,16 @@ class Bot {
   }
 
   async handleStep(step) {
-    console.log("Handling step:", step);
-
     switch (step.type) {
       case "message":
         await this.messageHandler.displayMessage(step.content, step.type);
         break;
       case "input":
-        await this.inputHandler.promptInput(step.mask, step.name);
+        var inputData = await this.inputHandler.promptInput(step.mask, step.name);
+        console.log(inputData);
+        this.userData[[step.name]] = inputData[step.name];
+        console.log(this.userData);
+        await this.messageHandler.displayMessage(inputData[step.name], step.type);
         break;
       case "select":
         // Implemente a lógica para manipular seleções aqui
@@ -143,10 +134,7 @@ class Bot {
 
 $(document).ready(() => {
   const messageHandler = new MessageHandler("#chat-body");
-  const inputHandler = new InputHandler("#message-input", "#send-button", {
-    userData: {},
-    messageHandler,
-  });
+  const inputHandler = new InputHandler("#message-input", "#send-button");
   const bot = new Bot(chatSequence, inputHandler, messageHandler);
 
   bot.init();
